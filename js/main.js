01a039f647e408e9912f97d720123f81128bfa73
stefanenache12 <stefanenache12@gmail.com>
2023-06-19 17:47:36 +0200

    ADD BUTTON FUNCTION TO ADD NEW ITEMS AT THE LIST

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const listaSpesa = ['Pasta','Uova','Latte','Pane','Frutta'];
 
 let listItems = document.getElementById('list');
 let buttonAddListItem = document.getElementById('add-button')
+let inputListItem = document.getElementById('add-list-item');
 
 let i = 0;
 
@@ -15,5 +16,17 @@ while(i < listaSpesa.length){
     i++;
 }
 
+buttonAddListItem.addEventListener('click',
+    function(){
 
+        let newListItem = inputListItem.value;
+        listaSpesa.push(newListItem);
+        listItems.innerHTML += `
+        <li class="list-group-item">
+            ${listaSpesa[listaSpesa.length - 1]}   
+        </li>
+        `;
+        inputListItem.value = '';
+    }
+)
 
